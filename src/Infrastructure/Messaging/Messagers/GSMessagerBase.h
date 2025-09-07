f0604e04e63798f83a58562a9e181d0d2e55ce23
sammich10 <sammymichelsen@gmail.com>
2025-09-07 14:35:08 -0500

    decrust

--- src/Infrastructure/Messaging/Messagers/GSMessagerBase.h
+++ src/Infrastructure/Messaging/Messagers/GSMessagerBase.h
@@ -8,18 +8,20 @@
 #include <thread>
 #include <atomic>
 
-namespace PiTrac {
-
-class ZmqMessenger {
-private:
-    void* context_;
-    void* socket_;
+namespace PiTrac
+{
+class ZmqMessenger
+{
+  private:
+    void *context_;
+    void *socket_;
     std::atomic<bool> running_;
     std::thread receive_thread_;
     std::function<void(std::unique_ptr<IMessage>)> message_handler_;
-    
-public:
-    enum class SocketType {
+
+  public:
+    enum class SocketType
+    {
         Publisher,
         Subscriber,
         Request,
@@ -27,15 +29,18 @@ public:
         Push,
         Pull
     };
-    
-    ZmqMessenger(SocketType type) : context_(nullptr), socket_(nullptr), running_(false) {
+
+    ZmqMessenger(SocketType type) : context_(nullptr), socket_(nullptr), running_(false)
+    {
         context_ = zmq_ctx_new();
-        if (!context_) {
+        if (!context_)
+        {
             throw std::runtime_error("Failed to create ZMQ context");
         }
-        
+
         int socket_type;
-        switch (type) {
+        switch (type)
+        {
             case SocketType::Publisher: socket_type = ZMQ_PUB; break;
             case SocketType::Subscriber: socket_type = ZMQ_SUB; break;
             case SocketType::Request: socket_type = ZMQ_REQ; break;
@@ -44,126 +49,153 @@ public:
             case SocketType::Pull: socket_type = ZMQ_PULL; break;
             default: throw std::invalid_argument("Invalid socket type");
         }
-        
+
         socket_ = zmq_socket(context_, socket_type);
-        if (!socket_) {
+        if (!socket_)
+        {
             zmq_ctx_destroy(context_);
             throw std::runtime_error("Failed to create ZMQ socket");
         }
     }
-    
-    ~ZmqMessenger() {
+
+    ~ZmqMessenger()
+    {
         stop();
-        if (socket_) {
+        if (socket_)
+        {
             zmq_close(socket_);
         }
-        if (context_) {
+        if (context_)
+        {
             zmq_ctx_destroy(context_);
         }
     }
-    
-    void bind(const std::string& endpoint) {
+
+    void bind(const std::string &endpoint)
+    {
         int rc = zmq_bind(socket_, endpoint.c_str());
-        if (rc != 0) {
+        if (rc != 0)
+        {
             throw std::runtime_error("Failed to bind to " + endpoint + ": " + zmq_strerror(errno));
         }
     }
-    
-    void connect(const std::string& endpoint) {
+
+    void connect(const std::string &endpoint)
+    {
         int rc = zmq_connect(socket_, endpoint.c_str());
-        if (rc != 0) {
-            throw std::runtime_error("Failed to connect to " + endpoint + ": " + zmq_strerror(errno));
+        if (rc != 0)
+        {
+            throw std::runtime_error("Failed to connect to " + endpoint + ": " +
+                                     zmq_strerror(errno));
         }
     }
-    
-    void subscribe(const std::string& topic = "") {
+
+    void subscribe(const std::string &topic = "")
+    {
         int rc = zmq_setsockopt(socket_, ZMQ_SUBSCRIBE, topic.c_str(), topic.length());
-        if (rc != 0) {
+        if (rc != 0)
+        {
             throw std::runtime_error("Failed to subscribe to topic: " + zmq_strerror(errno));
         }
     }
-    
-    void sendMessage(const IMessage& message) {
+
+    void sendMessage(const IMessage &message)
+    {
         zmq_msg_t msg;
         message.toZmqMessage(msg);
-        
+
         int rc = zmq_msg_send(&msg, socket_, 0);
-        if (rc < 0) {
+        if (rc < 0)
+        {
             zmq_msg_close(&msg);
             throw std::runtime_error("Failed to send message: " + std::string(zmq_strerror(errno)));
         }
-        
+
         zmq_msg_close(&msg);
     }
-    
-    void sendMessage(const IMessage& message, const std::string& topic) {
+
+    void sendMessage(const IMessage &message, const std::string &topic)
+    {
         // Send topic frame first
         zmq_msg_t topic_msg;
         zmq_msg_init_size(&topic_msg, topic.size());
         memcpy(zmq_msg_data(&topic_msg), topic.c_str(), topic.size());
         zmq_msg_send(&topic_msg, socket_, ZMQ_SNDMORE);
         zmq_msg_close(&topic_msg);
-        
+
         // Send message frame
         sendMessage(message);
     }
-    
+
     template<typename MessageType>
-    std::unique_ptr<MessageType> receiveMessage(int timeout_ms = -1) {
+    std::unique_ptr<MessageType> receiveMessage(int timeout_ms = -1)
+    {
         zmq_msg_t msg;
         zmq_msg_init(&msg);
-        
+
         // Set receive timeout if specified
-        if (timeout_ms >= 0) {
+        if (timeout_ms >= 0)
+        {
             zmq_setsockopt(socket_, ZMQ_RCVTIMEO, &timeout_ms, sizeof(timeout_ms));
         }
-        
+
         int rc = zmq_msg_recv(&msg, socket_, 0);
-        if (rc < 0) {
+        if (rc < 0)
+        {
             zmq_msg_close(&msg);
-            if (errno == EAGAIN) {
+            if (errno == EAGAIN)
+            {
                 return nullptr; // Timeout
             }
-            throw std::runtime_error("Failed to receive message: " + std::string(zmq_strerror(errno)));
+            throw std::runtime_error("Failed to receive message: " + std::string(zmq_strerror(
+                                                                                     errno)));
         }
-        
+
         auto message = std::make_unique<MessageType>();
         message->fromZmqMessage(msg);
         zmq_msg_close(&msg);
-        
+
         return message;
     }
-    
-    void startReceiving(std::function<void(std::unique_ptr<IMessage>)> handler) {
+
+    void startReceiving(std::function<void(std::unique_ptr<IMessage>)> handler)
+    {
         message_handler_ = handler;
         running_ = true;
         receive_thread_ = std::thread([this]() {
-            receiveLoop();
-        });
+                receiveLoop();
+            });
     }
-    
-    void stop() {
+
+    void stop()
+    {
         running_ = false;
-        if (receive_thread_.joinable()) {
+        if (receive_thread_.joinable())
+        {
             receive_thread_.join();
         }
     }
-    
-private:
-    void receiveLoop() {
-        while (running_) {
+
+  private:
+    void receiveLoop()
+    {
+        while (running_)
+        {
             zmq_msg_t msg;
             zmq_msg_init(&msg);
-            
+
             int timeout = 100; // 100ms timeout
             zmq_setsockopt(socket_, ZMQ_RCVTIMEO, &timeout, sizeof(timeout));
-            
+
             int rc = zmq_msg_recv(&msg, socket_, 0);
-            if (rc >= 0 && message_handler_) {
+            if (rc >= 0 && message_handler_)
+            {
                 // Here you'd need message type detection logic
                 // For now, this is a placeholder
                 zmq_msg_close(&msg);
-            } else if (errno != EAGAIN) {
+            }
+            else if (errno != EAGAIN)
+            {
                 zmq_msg_close(&msg);
                 break; // Error occurred
             }
@@ -171,7 +203,6 @@ private:
         }
     }
 };
-
 } // namespace PiTrac
 
 #endif // ZMQ_MESSENGER_H
--- src/Infrastructure/Messaging/Messages/GSMessageBase.h
+++ src/Infrastructure/Messaging/Messages/GSMessageBase.h
@@ -5,58 +5,60 @@
 #include <stdexcept>
 #include <sstream>
 
-namespace PiTrac {
-
-class GSMessageBase : public GSMessageInterface {
-   
-public:
+namespace PiTrac
+{
+class GSMessageBase : public GSMessageInterface
+{
+  public:
     GSMessageBase();
 
     virtual ~GSMessageBase() = default;
-    
+
     // Timestamp operations
     std::chrono::system_clock::time_point getTimestamp() const override;
-    
-    void setTimestamp(const std::chrono::system_clock::time_point& timestamp) override;
-    
+
+    void setTimestamp(const std::chrono::system_clock::time_point &timestamp) override;
+
     // ZMQ message operations implementation
-    void toZmqMessage(zmq_msg_t& msg) const override;
-    
-    void fromZmqMessage(zmq_msg_t& msg) override;
-    
+    void toZmqMessage(zmq_msg_t &msg) const override;
+
+    void fromZmqMessage(zmq_msg_t &msg) override;
+
     // Utility methods
     std::string toString() const override;
 
-protected:
+  protected:
     // Helper for serializing common fields
     template<typename Packer>
-    void packCommonFields(Packer& packer) const {
+    void packCommonFields(Packer &packer) const
+    {
         auto timestamp_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
             timestamp_.time_since_epoch()).count();
         packer.pack(getMessageType());
         packer.pack(timestamp_ms);
     }
-    
+
     // Helper for deserializing common fields
     template<typename Unpacker>
-    void unpackCommonFields(Unpacker& unpacker) {
+    void unpackCommonFields(Unpacker &unpacker)
+    {
         std::string message_type;
         int64_t timestamp_ms;
         unpacker.unpack(message_type);
         unpacker.unpack(timestamp_ms);
-        
-        if (message_type != getMessageType()) {
-            throw std::runtime_error("Message type mismatch: expected " + 
-                getMessageType() + ", got " + message_type);
+
+        if (message_type != getMessageType())
+        {
+            throw std::runtime_error("Message type mismatch: expected " +
+                                     getMessageType() + ", got " + message_type);
         }
-        
+
         timestamp_ = std::chrono::system_clock::time_point(
             std::chrono::milliseconds(timestamp_ms));
     }
 
     std::chrono::system_clock::time_point timestamp_;
 };
-
 } // namespace PiTrac
 
 #endif // MESSAGE_BASE_H
