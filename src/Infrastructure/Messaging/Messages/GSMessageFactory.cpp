d20fbe21600099190366e57da4f9631eef982cc1
sammich10 <sammymichelsen@gmail.com>
2025-09-07 14:55:57 -0500

    split implementation

---
+++ src/Infrastructure/Messaging/Messages/GSMessageFactory.cpp
@@ -0,0 +1,44 @@
+#include "Infrastructure/Messaging/Messages/GSMessageFactory.h"
+
+namespace PiTrac
+{
+GSMessageFactory::GSMessageFactory()
+{
+    // Register message types
+}
+
+std::unique_ptr<GSMessageInterface> GSMessageFactory::createFromZmqMessage(zmq_msg_t &msg)
+{
+    // Extract message type from the beginning of the message
+    const char *data = static_cast<const char *>(zmq_msg_data(&msg));
+    size_t size = zmq_msg_size(&msg);
+
+    if (size == 0)
+    {
+        throw std::runtime_error("Empty message received");
+    }
+
+    // Unpack just the first element to get message type
+    msgpack::object_handle oh = msgpack::unpack(data, size);
+    msgpack::object obj = oh.get();
+
+    if (obj.type != msgpack::type::ARRAY || obj.via.array.size == 0)
+    {
+        throw std::runtime_error("Invalid message format");
+    }
+
+    std::string message_type;
+    obj.via.array.ptr[0].convert(message_type);
+
+    auto it = creators_.find(message_type);
+    if (it == creators_.end())
+    {
+        throw std::runtime_error("Unknown message type: " + message_type);
+    }
+
+    auto message = it->second();
+    message->fromZmqMessage(msg);
+    return message;
+}
+
+} // namespace PiTrac
--- src/Infrastructure/Messaging/Messages/GSMessageFactory.h
+++ src/Infrastructure/Messaging/Messages/GSMessageFactory.h
@@ -1,24 +1,17 @@
 #ifndef MESSAGE_FACTORY_H
 #define MESSAGE_FACTORY_H
 
-#include "IMessage.h"
-#include "Infrastructure/Messages/Messages/*.h"
+#include "Infrastructure/Messaging/GSMessageInterface.h"
 #include <memory>
 #include <unordered_map>
 #include <functional>
 
 namespace PiTrac
 {
-class MessageFactory
+class GSMessageFactory
 {
-  private:
-    std::unordered_map<std::string, std::function<std::unique_ptr<IMessage>()> > creators_;
-
   public:
-    MessageFactory()
-    {
-        // Register message types
-    }
+    GSMessageFactory();
 
     template<typename MessageType>
     void registerMessage(const std::string &type)
@@ -28,39 +21,12 @@ class MessageFactory
                           };
     }
 
-    std::unique_ptr<IMessage> createFromZmqMessage(const zmq_msg_t &msg)
-    {
-        // Extract message type from the beginning of the message
-        const char *data = static_cast<const char *>(zmq_msg_data(&msg));
-        size_t size = zmq_msg_size(&msg);
+    std::unique_ptr<GSMessageInterface> createFromZmqMessage(zmq_msg_t &msg);
 
-        if (size == 0)
-        {
-            throw std::runtime_error("Empty message received");
-        }
+  private:
 
-        // Unpack just the first element to get message type
-        msgpack::object_handle oh = msgpack::unpack(data, size);
-        msgpack::object obj = oh.get();
-
-        if (obj.type != msgpack::type::ARRAY || obj.via.array.size == 0)
-        {
-            throw std::runtime_error("Invalid message format");
-        }
-
-        std::string message_type;
-        obj.via.array.ptr[0].convert(message_type);
-
-        auto it = creators_.find(message_type);
-        if (it == creators_.end())
-        {
-            throw std::runtime_error("Unknown message type: " + message_type);
-        }
-
-        auto message = it->second();
-        message->fromZmqMessage(msg);
-        return message;
-    }
+    std::unordered_map<std::string,
+                       std::function<std::unique_ptr<GSMessageInterface>()> > creators_;
 };
 } // namespace PiTrac
 
